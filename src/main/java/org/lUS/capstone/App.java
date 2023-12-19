ec89f98a4c077867ef6c6e1297b1e3f6f247f6b2
vv2 <veena_v@intuit.com>
2023-12-19 11:18:29 +0530

    first commint

---
+++ src/main/java/org/lUS/capstone/App.java
@@ -0,0 +1,13 @@
+package org.lUS.capstone;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
